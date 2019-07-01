1c80f6f2c460f06b07f73aa75f286421adb05584
Romain Hunault <romain.hunault@e.email>
2019-07-01 10:05:52 +0000

    Fix SafetyNet default prefs

--- play-services-core/src/main/java/org/microg/gms/snet/SafetyNetPrefs.java
+++ play-services-core/src/main/java/org/microg/gms/snet/SafetyNetPrefs.java
@@ -57,7 +57,7 @@ public class SafetyNetPrefs implements SharedPreferences.OnSharedPreferenceChang
 
     public void update() {
         disabled = defaultPreferences.getBoolean(PREF_SNET_DISABLED, false);
-        official = defaultPreferences.getBoolean(PREF_SNET_OFFICIAL, false);
+        official = defaultPreferences.getBoolean(PREF_SNET_OFFICIAL, true);
         selfSigned = defaultPreferences.getBoolean(PREF_SNET_SELF_SIGNED, false);
         thirdParty = defaultPreferences.getBoolean(PREF_SNET_THIRD_PARTY, false);
         customUrl = defaultPreferences.getString(PREF_SNET_CUSTOM_URL, null);
